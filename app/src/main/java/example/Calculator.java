1abcfc72a212993b1b9713036fabc1735455c3b5
teresaguo <teresayuxin.guo@student.unimelb.edu.au>
2025-03-05 11:50:01 +1100

    update add and minus

--- app/src/main/java/example/Calculator.java
+++ app/src/main/java/example/Calculator.java
@@ -3,12 +3,14 @@ package example;
 public class Calculator {
     public int add(int a, int b) {
         // TODO: Implement this method
-        return 4;
+        int i = a + b;
+        return i;
     }
 
     public int minus(int a, int b) {
         // TODO: Implement this method
-        return 10;
+        int i = a - b;
+        return i;
     }
 
     public int multiply(int a, int b) {
